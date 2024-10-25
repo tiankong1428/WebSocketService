7f5b11b8bc9e0bd8f1fe9afea51157bc37ae1d65
tiankong1428 <3166140298@qq.com>
2024-10-25 20:19:34 +0800

    更新 AppCompatTextView.kt

--- app/src/main/java/studio/attect/websocketservice/example/AppCompatTextView.kt
+++ app/src/main/java/studio/attect/websocketservice/example/AppCompatTextView.kt
@@ -1,9 +1,5 @@
 package studio.attect.websocketservice.example
 
-import android.content.ClipData
-import android.content.ClipboardManager
-import android.content.Context
-import android.widget.Toast
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.core.content.ContextCompat
 
@@ -17,16 +13,4 @@ fun AppCompatTextView.disableClick(){
     setTextColor(ContextCompat.getColor(context, R.color.colorDisable))
     isFocusable = false
     isClickable = false
-}
-
-fun AppCompatTextView.enableClickCopy() {
-    this.setOnClickListener {
-        // 复制文本到剪贴板
-        val clipboard = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-        val clip = ClipData.newPlainText("Copied Text", this.text)
-        clipboard.setPrimaryClip(clip)
-
-        // 提示用户已复制
-        Toast.makeText(context, "文本已复制", Toast.LENGTH_SHORT).show()
-    }
 }
