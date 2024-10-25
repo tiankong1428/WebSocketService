247f059cc98fbb05369a2bc592d0a8e1274b65fc
tiankong1428 <3166140298@qq.com>
2024-10-25 19:49:56 +0800

    更新 AppCompatTextView.kt

--- app/src/main/java/studio/attect/websocketservice/example/AppCompatTextView.kt
+++ app/src/main/java/studio/attect/websocketservice/example/AppCompatTextView.kt
@@ -19,9 +19,8 @@ fun AppCompatTextView.disableClick(){
     isClickable = false
 }
 
-fun AppCompatTextView.enableLongClickCopy() {
-    this.isLongClickable = true
-    this.setOnLongClickListener {
+fun AppCompatTextView.enableClickCopy() {
+    this.setOnClickListener {
         // 复制文本到剪贴板
         val clipboard = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
         val clip = ClipData.newPlainText("Copied Text", this.text)
@@ -29,6 +28,5 @@ fun AppCompatTextView.enableLongClickCopy() {
 
         // 提示用户已复制
         Toast.makeText(context, "文本已复制", Toast.LENGTH_SHORT).show()
-        true  // 返回 true 表示事件已处理
     }
 }
